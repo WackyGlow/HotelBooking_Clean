fabfd69f1a882a163e53dcd3636d21173e47c85e
WackyGlow <70566908+WackyGlow@users.noreply.github.com>
2023-10-03 09:52:45 +0200

    Update ServiceTests.cs

--- HotelBooking.IntegrationTests/CoreTests/ServiceTests.cs
+++ HotelBooking.IntegrationTests/CoreTests/ServiceTests.cs
@@ -8,8 +8,45 @@ namespace HotelBooking.IntegrationTests.CoreTests;
 
 public class ServiceTests
 {
-    [Fact]
-    public void CreateBooking_SuccessfulBooking()
+    public static IEnumerable<object[]> BookingData =>
+        new List<object[]>
+        {
+            // Test case 1: Successful booking
+            new object[] {
+                DateTime.Today.AddDays(1),
+                DateTime.Today.AddDays(3),
+                new List<Room> { new Room { Id = 1 } },
+                new List<Booking>
+                {
+                    new Booking
+                    {
+                        RoomId = 1,
+                        StartDate = DateTime.Today.AddDays(5),
+                        EndDate = DateTime.Today.AddDays(7),
+                        IsActive = true,
+                    }
+                },
+                true
+            },
+
+            // Test case 2: No available room
+            new object[] {
+                DateTime.Today.AddDays(1),
+                DateTime.Today.AddDays(3),
+                new List<Room>(),
+                new List<Booking>(),
+                false
+            },
+        };
+
+    [Theory]
+    [MemberData(nameof(BookingData))]
+    public void CreateBooking_ReturnsExpectedResult(
+        DateTime startDate,
+        DateTime endDate,
+        List<Room> availableRooms,
+        List<Booking> existingBookings,
+        bool expectedResult)
     {
         // Arrange
         var bookingRepositoryMock = new Mock<IRepository<Booking>>();
@@ -18,56 +55,27 @@ public class ServiceTests
 
         var booking = new Booking
         {
-            StartDate = DateTime.Today.AddDays(1),
-            EndDate = DateTime.Today.AddDays(3),
+            StartDate = startDate,
+            EndDate = endDate,
         };
 
-        var room = new Room { Id = 1 };
-        roomRepositoryMock.Setup(repo => repo.GetAll()).Returns(new List<Room> { room });
+        roomRepositoryMock.Setup(repo => repo.GetAll()).Returns(availableRooms);
+        bookingRepositoryMock.Setup(repo => repo.GetAll()).Returns(existingBookings);
 
-        bookingRepositoryMock.Setup(repo => repo.GetAll()).Returns(new List<Booking>
+        if (expectedResult)
         {
-            new Booking
-            {
-                RoomId = 1,
-                StartDate = DateTime.Today.AddDays(5),
-                EndDate = DateTime.Today.AddDays(7),
-                IsActive = true,
-            }
-        });
-
-        bookingRepositoryMock.Setup(repo => repo.Add(booking));
+            bookingRepositoryMock.Setup(repo => repo.Add(booking));
+        }
 
         // Act
         var result = bookingManager.CreateBooking(booking);
 
         // Assert
-        Assert.True(result);
-        Assert.Equal(1, booking.RoomId);
-        bookingRepositoryMock.Verify(repo => repo.Add(booking), Times.Once);
-    }
-
-    [Fact]
-    public void CreateBooking_NoAvailableRoom()
-    {
-        // Arrange
-        var bookingRepositoryMock = new Mock<IRepository<Booking>>();
-        var roomRepositoryMock = new Mock<IRepository<Room>>();
-        var bookingManager = new BookingManager(bookingRepositoryMock.Object, roomRepositoryMock.Object);
-
-        var booking = new Booking
+        Assert.Equal(expectedResult, result);
+        if (expectedResult)
         {
-            StartDate = DateTime.Today.AddDays(1),
-            EndDate = DateTime.Today.AddDays(3),
-        };
-
-        roomRepositoryMock.Setup(repo => repo.GetAll()).Returns(new List<Room>());
-        bookingRepositoryMock.Setup(repo => repo.GetAll()).Returns(new List<Booking>());
-
-        // Act
-        var result = bookingManager.CreateBooking(booking);
-
-        // Assert
-        Assert.False(result);
+            bookingRepositoryMock.Verify(repo => repo.Add(booking), Times.Once);
+        }
     }
+
 }
