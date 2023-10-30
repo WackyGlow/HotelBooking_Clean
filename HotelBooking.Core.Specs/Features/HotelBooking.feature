b2be35c61beea38642f6c0cc070d7f096019bf69
Rasmus Thyregod Kristensen <70566908+WackyGlow@users.noreply.github.com>
2023-10-30 22:36:31 +0100

    AddedSpecFlow?

---
+++ HotelBooking.Core.Specs/Features/HotelBooking.feature
@@ -0,0 +1,33 @@
+﻿@CreateBooking
+Feature: Creating a Booking
+        In order to book a room
+        As a hotel guest
+        I want to create a booking
+
+Scenario: Successful booking creation
+        Given I have selected a valid start date
+        And I have selected a valid end date
+        When I request to create a booking
+        Then the booking should be created successfully
+
+@InvalidStartDate
+Scenario: Booking with an invalid start date
+        Given I have selected an invalid start date
+        And I have selected a valid end date
+        When I request to create a booking
+        Then the booking should not be created
+
+@InvalidEndDate
+Scenario: Booking with an invalid end date
+        Given I have selected a valid start date
+        And I have selected an invalid end date
+        When I request to create a booking
+        Then the booking should not be created
+
+@NoRoomsAvailable
+Scenario: Booking when no rooms are available
+        Given there are no available rooms
+        And I have selected a valid start date
+        And I have selected a valid end date
+        When I request to create a booking
+        Then the booking should not be created
---
+++ HotelBooking.Core.Specs/StepDefinitions/CreateBookingSteps.cs
@@ -0,0 +1,41 @@
+﻿
+namespace HotelBooking.Core.Specs.StepDefinitions
+{
+    [Binding]
+    public class CreateBookingSteps
+    {
+        private Booking booking;
+        private bool bookingResult;
+        private HotelBookingCore hotelBooking;
+
+        public CreateBookingSteps()
+        {
+            // Set up any necessary context here
+            hotelBooking = new HotelBookingCore();
+        }
+
+        [Given("I have selected a valid start date")]
+        public void GivenIHaveSelectedAValidStartDate()
+        {
+            booking = new Booking
+            {
+                StartDate = DateTime.Now.AddDays(1), // Set a valid start date
+                EndDate = DateTime.Now.AddDays(3)    // Set a valid end date
+            };
+        }
+
+        [When("I request to create a booking")]
+        public void WhenIRequestToCreateABooking()
+        {
+            bookingResult = hotelBooking.CreateBooking(booking);
+        }
+
+        [Then("the booking should be created successfully")]
+        public void ThenTheBookingShouldBeCreatedSuccessfully()
+        {
+            Assert.IsTrue(bookingResult, "Booking should be created successfully");
+        }
+
+        // Implement similar step definitions for the other scenarios
+    }
+}
